eb9a316dee90035f44231becbbce10e44d7f9251
Fyced <96468855+Fyced@users.noreply.github.com>
2023-05-10 13:12:34 +0200

    Add files via upload

---
+++ HTML/juego.js
@@ -0,0 +1,14 @@
+var formulaReactivo = [];
+var formulaProducto = [];
+
+function dibujarReactivos(numero){
+var n = numero;
+
+var alto = 50;
+var largo = 70;
+
+}
+
+function guardarElementos(){
+
+}
