098121ed461d3b3b13f9e9a75fca2f650076cca3
Fyced <96468855+Fyced@users.noreply.github.com>
2023-05-10 19:30:09 +0200

    Update juego.js

--- HTML/juego.js
+++ HTML/juego.js
@@ -1,5 +1,6 @@
-var elementos = ["H","Li","Na","K","Rb","Cs","Fr","Be","Mg","Ca","Sr","Ba","Ra"];
-var formulaReactivo = [];
+var elementos = ["H","Li","Na","K","Rb","Cs","Fr","Be","Mg","Ca","Sr","Ba","Ra","Sc","Ti","V","Cr","Mn","Fe","Co","Ni","Cu"];
+var elementosFormula = [];
+var formulaReactivo = ["Na2CrO3","H2","Cu2"];
 var formulaProducto = [];
 
 function dibujarReactivos(numero){
@@ -11,5 +12,11 @@ var largo = 70;
 }
 
 function guardarElementos(){
-
+  for(let e = 0; e<=formulaReactivo.length; e++){
+for(let i = 0; i<=elementos.length;i++){
+  if(formulaReactivo[e].includes(elementos[i])==true){
+    elementosFormula.push(elementos[i]);
+  }
+}
+}
 }
