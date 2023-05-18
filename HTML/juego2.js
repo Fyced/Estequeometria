236e676cd02b943f31db6ef3fadfc3beef89155d
Fyced <96468855+Fyced@users.noreply.github.com>
2023-05-18 13:16:38 +0200

    Actualizar juego2.js

--- HTML/juego2.js
+++ HTML/juego2.js
@@ -1,1 +1,17 @@
 //Poner un botón submit para cambiar de fórmula química una vez ya se ha hecho 
+int elementosFormula=[];
+
+
+function guardarElementos(e){
+elementosFormula.push(e);
+
+estequiometría();
+}
+
+function estequiometría(){
+for(let i=0; i<elementosFormula.length;i++){
+for(let e =0; e<elementosFormula length;e++){
+if(elementosFormula[i]
+}
+}
+}
